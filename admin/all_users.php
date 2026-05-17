2a3f5a0afbfb92d9f78995ee3bc78ddb386bb515
zkom01 <komarek.zdenek@seznam.cz>
2026-05-18 00:43:22 +0200

    feat: přidání zobrazení e-mailu do seznamu uživatelů

--- admin/all_users.php
+++ admin/all_users.php
@@ -50,6 +50,7 @@
                             <?= htmlspecialchars($one_user['first_name']) . " " . htmlspecialchars($one_user['second_name']) ?>
                         </h2>
                         <p>Práva: <?= htmlspecialchars($one_user['role'])?></p>
+                        <p><?= htmlspecialchars($one_user['email'])?></p>
                         <section class="buttons-container">
                             <a href="one_user.php?id=<?= $one_user['id'] ?>" class="btn btn-primary">Detail</a>
                         </section>
